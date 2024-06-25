9f1f91305a246176c6774af35220be843cc3ad48
Chris Pierson <cpierswim@gmail.com>
2024-06-24 23:16:12 -0400

    NEW: Changed layout on coach dramatically

---
+++ backend/WebSocket-SendSplit.py
@@ -0,0 +1,60 @@
+import json
+import boto3
+
+client = boto3.client('apigatewaymanagementapi', endpoint_url="https://mjim68pp6h.execute-api.us-east-2.amazonaws.com/production/")
+
+s3 = boto3.client('s3')
+BUCKET_NAME = "swimcalculator"
+BUCKET_PREFIX = "rooms/"
+
+def lambda_handler(event, context):
+    print(event)
+    
+    print("Getting all connections")
+   
+    body = json.loads(event['body'])
+    room_key = body["roomKey"]
+
+    key_file_name = BUCKET_PREFIX + room_key + ".json"
+
+    print("Accessing file: " + key_file_name)
+    
+    response = ""
+
+    try:
+        response = s3.get_object(Bucket=BUCKET_NAME, Key=key_file_name)
+    except:
+        return {
+            'statusCode': 404,
+            'message': "room not found"
+        }
+    
+    print("Room Key Found:")
+    file_contents = response["Body"]
+    as_string = file_contents.read().decode('utf-8')
+    room_data = json.loads(as_string)
+
+    print("Room Data:")
+    print(room_data["room_key"])
+   
+    lane_to_send_to = body["lane_to_send_to"]
+    split = body["split"]
+    connected_lanes = room_data["connected_lanes"]
+
+    lane_to_send_to = str(lane_to_send_to)
+    print(connected_lanes)
+
+    connection_id = connected_lanes[lane_to_send_to]
+    print("Sending message to lane " + lane_to_send_to + " connectionID: " + connection_id)
+    message_to_send = {
+        "type": "split",
+        "split": split
+    }
+    print("Split " + split + " sent")
+    client.post_to_connection(
+        Data=json.dumps(message_to_send).encode('utf-8'),
+        ConnectionId=connection_id
+    )
+   
+   
+    return { "statusCode": 200  }
